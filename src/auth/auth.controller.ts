856389a612f2b419479d9608e5001d6ea28b7dd7
syedomer17 <syedomerali2006@gmail.com>
2025-05-23 14:02:49 +0530

    signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { signupDto } from './dto/signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  // POST auth/signup
+  @Post('signup')
+  async signUp(@Body() signupData: signupDto) {
+    return this.authService.signup(signupData)
+    
+  }
+  
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { signupDto } from './dto/signup.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
+
+  async signup(signupData: signupDto) {
+
+    const {email,  password, name} = signupData;
+    // check if email is in use
+    const emailInUse = await this.UserModel.findOne({email : email,});
+
+    if(emailInUse){
+        throw new BadRequestException('Email already in use!')
+    }
+    // hask password
+    const hashPassword = await bcrypt.hash(password, 10);
+    // create user document and save the user in the db
+    await this.UserModel.create({
+        name,
+        email,
+        password : hashPassword
+    })
+  }
+}
